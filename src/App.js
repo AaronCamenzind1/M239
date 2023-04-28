a3ca72e6867291397af701a17f94208b893d55d5
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-04-28 11:16:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://joker-it.ch/wp-content/uploads/2019/03/bbzw.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BBZW Meme Seite
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
