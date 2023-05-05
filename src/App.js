fcf32fa22b985dc11c1acbb3e923c9a4fee39436
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-05-05 10:33:13 +0200

    added tailwind code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 
 function App() {
   return (
+    <script src="https://cdn.tailwindcss.com"></script>
     <div className="App">
       <header className="App-header">
         
         <img src="https://joker-it.ch/wp-content/uploads/2019/03/bbzw.jpg" className="App-logo" alt="logo" />
         
         <h1>BBZW Hall of fame</h1>
-        
+        <br>
+        <br>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/img/card-top.jpg" alt="Meme">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+              Meme Beschreibung
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bbzw</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#sursee</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#lol</span>
+          </div>
+        </div>
       </header>
     </div>
   );
